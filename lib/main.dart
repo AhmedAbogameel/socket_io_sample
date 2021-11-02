dbec033ec62b9c3e1305c6078adfce8e298c4b11
AhmedAbogameel <gamilco81@gmail.com>
2021-11-02 15:45:27 +0200

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,93 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:socket_io_client/socket_io_client.dart' as IO;
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: HomeView(),
+    );
+  }
+}
+
+class HomeView extends StatefulWidget {
+  @override
+  State<HomeView> createState() => _HomeViewState();
+}
+
+class _HomeViewState extends State<HomeView> {
+  List<String> messages = [];
+  TextEditingController textEditingController = TextEditingController();
+
+  void connect() async {
+    print("hello");
+
+    IO.Socket socket = IO.io('https://appio-chat.herokuapp.com/clients');
+    socket.onConnect((_) {
+      print('connect');
+      socket.emit('joinRoom', 'clients-chat');
+    });
+    socket.on('client chat message', (d) => print(d));
+
+    Future.delayed(Duration(seconds: 3), () {
+      print(socket.connected);
+    });
+  }
+
+  @override
+  void initState() {
+    connect();
+    super.initState();
+  }
+
+  void sendMessage() {
+    messages.insert(0, textEditingController.text);
+    textEditingController.clear();
+    setState(() {});
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.all(20),
+        child: Column(
+          children: [
+            _messagesList(),
+            _sendField(),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _messagesList() => Expanded(
+        child: ListView.separated(
+          separatorBuilder: (context, index) => Divider(thickness: 2),
+          reverse: true,
+          itemBuilder: (context, index) => ListTile(
+            title: Text(messages[index]),
+          ),
+          itemCount: messages.length,
+        ),
+      );
+
+  Widget _sendField() => Row(
+        children: [
+          Expanded(
+            child: TextFormField(
+              controller: textEditingController,
+            ),
+          ),
+          IconButton(
+            icon: Icon(Icons.send),
+            onPressed: sendMessage,
+          ),
+        ],
+      );
+}
